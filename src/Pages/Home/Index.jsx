5d2e15e4f8560bd110695e16cb350c6a45687f3a
[adityasharma002] <[adityasharma2621@gmail.com]>
2024-05-26 16:59:33 +0530

    removed AppDrawer

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,15 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Box, NativeBaseProvider, Text } from 'native-base';
 import React from 'react';
-import AppDrawer from '../../Common/AppDrawer';
 import { NavigationContainer } from '@react-navigation/native';
-import AppBottomNav from '../../Common/AppBottomNav';
 
 const Home = () => {
     return (
         <NavigationContainer independent={true}>
             <NativeBaseProvider>
-                <><AppDrawer /><AppBottomNav /><Text>home</Text></>
+                <Text>home</Text>
                 {/* <AppDrawer /> */}
             </NativeBaseProvider >
         </NavigationContainer >
